ee2a522bb32912c58387015a14ea6014b85ed871
v-yakushin-exceed <v.yakushin.exceedteam@gmail.com>
2019-11-20 14:58:29 +0300

    fixed var on const

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ $(document).ready(function(){
       function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
       }
-      var random = getRandomInt(1, 7);
+      const random = getRandomInt(1, 7);
       
       getRandomInt(10, 20);
-      var text = $('#text').val();
+      const text = $('#text').val();
         $('ul.header__items').append('<li class="header__link header__link-bg'+ random +'"><div class="header__checkbox"><input id="checkbox" type="checkbox" name="check1" value="1"></div><div class="header__text"><h2> ' + text + ' </h2></div></li>').appendTo('ul.header__items');
       
     // second task
@@ -45,14 +45,14 @@ $(document).ready(function(){
       (function($) {
         $.fn.removeClassWild = function(mask) {
             return this.removeClass(function(index, cls) {
-                var re = mask.replace(/\*/g, '\\S+');
+                const re = mask.replace(/\*/g, '\\S+');
                 return (cls.match(new RegExp('\\b' + re + '', 'g')) || []).join(' ');
             });
         };
     })(jQuery);
 
       $('.piker__link').click(function() {
-        var click = $(this).attr('id');
+        const click = $(this).attr('id');
         $('.active').removeClassWild('active*');
         $('.active').removeClassWild('header__link-bg*');
         $('.active').addClass('active'+ click +'');
